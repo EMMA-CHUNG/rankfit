cb7df0a51addd775a9ef0ff367b6ccdbba78f3f1
EMMA-CHUNG <155692197+EMMA-CHUNG@users.noreply.github.com>
2025-08-31 21:49:14 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rankfit",
+    version="0.1.0",
+    author="Emma Chung",
+    author_email="hsiaoyin.chung@gmail.com",
+    description="Segment-level ranking quality metrics for machine learning models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EMMA-CHUNG/rankfit",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.19.0",
+        "pandas>=1.3.0",
+        "scipy>=1.7.0",
+        "matplotlib>=3.4.0",
+        "scikit-learn>=0.24.0",
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/EMMA-CHUNG/rankfit/issues",
+        "Source": "https://github.com/EMMA-CHUNG/rankfit",
+        "Documentation": "https://github.com/EMMA-CHUNG/rankfit#readme",
+    },
+)
